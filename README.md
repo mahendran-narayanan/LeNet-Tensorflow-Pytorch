3e797e44fa9ce48d9718db1c863b77607557572b
mahendran-narayanan <mahendranNNM@gmail.com>
2023-10-11 18:47:26 +0530

    Lenet model

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # LeNet-Tensorflow-Pytorch
 Create LeNet model including Tensorflow and Pytorch packages
+
+```
+usage: main.py [-h] [--model {tf,torch}]
+
+Create LeNet model in Tensorflow or Pytorch package
+
+optional arguments:
+  -h, --help          show this help message and exit
+  --model {tf,torch}  Model will be created on Tensorflow, Pytorch (default: Tensorflow)
+```
+
+To create model in Tensorflow:
+
+```
+python3 main.py --model tf
+```
+
+To create model in Pytorch:
+
+```
+python3 main.py --model torch
+```
+
+![alt text](Pictures/Lenet-5.png)
+
+Paper link: [Lenet paper](http://vision.stanford.edu/cs598_spring07/papers/Lecun98.pdf)
---
+++ main.py
@@ -0,0 +1,77 @@
+import argparse
+import tensorflow as tf
+import torch
+
+
+class LeNet_tf(tf.keras.Model):
+	def __init__(self):
+		super().__init__()
+		self.conv1 = tf.keras.layers.Conv2D(6,5,strides=1,activation='relu')
+		self.maxpool1 = tf.keras.layers.MaxPooling2D(pool_size=(2,2))
+		self.conv2 = tf.keras.layers.Conv2D(16,5,activation='relu')
+		self.maxpool2 = tf.keras.layers.MaxPooling2D(pool_size=(2,2))
+		self.flat = tf.keras.layers.Flatten()
+		self.dense1 = tf.keras.layers.Dense(120,activation='relu')
+		self.dense2 = tf.keras.layers.Dense(84,activation='relu')
+		self.classifier = tf.keras.layers.Dense(10,activation='sigmoid')
+
+	def call(self,x):
+		x = self.maxpool1(self.conv1(x))
+		x = self.maxpool2(self.conv2(x))
+		x = self.flat(x)
+		x = self.dense1(x)
+		x = self.dense2(x)
+		x = self.classifier(x)
+		return x
+
+class LeNet_torch(torch.nn.Module):
+	def __init__(self):
+		super().__init__()
+		self.conv1 = torch.nn.Conv2d(1, 6, kernel_size=5)
+		self.relu1 = torch.nn.ReLU()
+		self.maxpool1 = torch.nn.MaxPool2d(2,2)
+		self.conv2 = torch.nn.Conv2d(6, 16, kernel_size=5)
+		self.relu2 = torch.nn.ReLU()
+		self.maxpool2 = torch.nn.MaxPool2d(2,2)
+		self.flatten = torch.nn.Flatten()
+		self.dense1 = torch.nn.Linear(16*16,120) # 4*4*16
+		self.dense2 = torch.nn.Linear(120,84)
+		self.classifier = torch.nn.Linear(84,10)
+		
+	def forward(self, x):
+		x = self.relu1(self.conv1(x))
+		x = self.maxpool1(x)
+		x = self.relu2(self.conv2(x))
+		x = self.maxpool2(x)
+		x = self.flatten(x)
+		x = self.dense1(x)
+		x = self.dense2(x)
+		x = self.classifier(x)
+		return x
+
+def model_torch():
+	return LeNet_torch()
+
+def model_tf():
+	return LeNet_tf()
+
+def main(args):
+	if args.model=='tf':
+		print('Model will be created in Tensorflow')
+		model = model_tf()
+		model.build(input_shape=(None,28,28,1))
+		model.summary()
+	else:
+		print('Model will be created in Pytorch')
+		model = model_torch()
+		print(model)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Create LeNet model in Tensorflow or Pytorch')
+	parser.add_argument('--model',
+	                    default='tf',
+	                    choices=['tf', 'torch'],
+	                    help='Model will be created on Tensorflow, Pytorch (default: %(default)s)')
+	args = parser.parse_args()
+	main(args)
